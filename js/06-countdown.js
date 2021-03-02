abd534b52bd3fc688825efc905783d07f9e99b55
OlenaZh <alyona.mesh@gmail.com>
2021-03-02 12:55:56 -0800

    countdown done

--- js/06-countdown.js
+++ js/06-countdown.js
@@ -1,5 +1,5 @@
 /*eslint-env browser*/
 var number = prompt ("Please enter number");
-for (number; number >= 0; number-=1) {
+for (number; number >= 0; number--) {
     document.write (number + "<br>")
 }
