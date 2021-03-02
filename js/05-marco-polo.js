b38be408ba9211119b358ed44adfe4638b22da90
OlenaZh <alyona.mesh@gmail.com>
2021-03-02 12:40:16 -0800

    Marco Polo Done

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -2,11 +2,13 @@
 
 var number = 1;
 for (number = 1; number <= 100; number+= 1) {
- if (number % 3 === 0) {
- document.write("Marco!" + "<br>");
-} if (number % 5 === 0) {
-    document.write("Polo!" + "<br>");
+ if (number % 3 === 0 && number % 5 === 0) {
+        document.write("Marco! Polo!" + "<br>");
+} else if (number % 3 === 0) {
+    document.write("Marco!" + "<br>");
+} else if (number % 5 === 0) {
+       document.write("Polo!" + "<br>");
 } else {
-    document.write(number + "<br>");
-}
+       document.write(number + "<br>");
+   }
 }
