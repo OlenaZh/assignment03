6a429c035fb1a660fa972eba762156560b5fc6cb
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 21:38:29 -0800

    coinFlip2 comment added

--- js/02-coinflip-streak.js
+++ js/02-coinflip-streak.js
@@ -1,3 +1,15 @@
 /*eslint-env browser*/
 var coinFlip;
-var coinFlip = Math.round(Math.random()) +1;
+ do {
+    coinFlip = Math.floor(Math.random() * 2);
+    if (coinFlip === 0) {
+        document.write("Heads" + "<br>");
+ 
+    } if (coinFlip === 1) {
+        document.write("Tails" + "<br>");
+ }
+} while (coinFlip === 1);
+
+
+
+/*var coinFlip = Math.round(Math.random()) +1;*/
