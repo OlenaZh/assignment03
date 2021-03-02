2973afef091f0d727ddcc12fa5013e5d555005c9
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 19:10:18 -0800

    odd even while added

--- js/04-odd-or-even.js
+++ js/04-odd-or-even.js
@@ -1,5 +1,11 @@
 /*eslint-env browser*/
-var number;
+/*var number = 0;
 for (var number = 0; number <= 15; number+= 1) {
     document.write(number + "<br>");
+}*/
+
+var number = 0;
+ while (number <= 15) {
+    number+= 1;
+    document.write("odd" +number+ "<br>");
 }
