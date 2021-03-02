8a0ac0e9fe97e7f4106bb9633a1a673fd4690465
OlenaZh <alyona.mesh@gmail.com>
2021-03-02 12:13:00 -0800

    Marco Polo step 2

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -4,6 +4,8 @@ var number = 1;
 for (number = 1; number <= 100; number+= 1) {
  if (number % 3 === 0) {
  document.write("Marco!" + "<br>");
+} if (number % 5 === 0) {
+    document.write("Polo!" + "<br>");
 } else {
     document.write(number + "<br>");
 }
