9fa4eaf0abdeed4cfc7d92ce76206992a76a7f9d
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 17:22:27 -0800

    coinFlip1 canges

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -6,7 +6,7 @@ var coinFlip = Math.round(Math.random()) +1;
 for (i =  0; i < 5; i+= 1) {
     if (coinFlip === 0) {
         document.write("Heads");
- break;
+ 
     } if (coinFlip === 1) {
         document.write("Tails");
 }
