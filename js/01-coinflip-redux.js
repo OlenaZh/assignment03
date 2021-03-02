a53bfc6c052d24700d3e80d98a836db6f6c8bed2
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 17:01:53 -0800

    coinFlipFor added

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,13 @@
+/*eslint-env browser*/
+var coinFlip;
+var i;
+var choice;
+var coinFlip = Math.round(Math.random()) +1;
+for (i =  0; i < choice.length; i+= 1) {
+    if (coinFlip === 0) {
+        document.write("Heads");
+
+    } if (coinFlip === 1) {
+        document.write("Tails");
+}
+}
