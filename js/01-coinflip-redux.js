0615315a13700f6d673a1f0eefa9e529e57d6f51
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 17:19:00 -0800

    coinflip break adde

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -3,10 +3,10 @@ var coinFlip;
 var i;
 var choice;
 var coinFlip = Math.round(Math.random()) +1;
-for (i =  0; i < choice.length; i+= 1) {
+for (i =  0; i < 5; i+= 1) {
     if (coinFlip === 0) {
         document.write("Heads");
-
+ break;
     } if (coinFlip === 1) {
         document.write("Tails");
 }
