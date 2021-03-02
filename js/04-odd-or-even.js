5f9e0a721224a5e71bb3d653b5eda776c5af8a4c
OlenaZh <alyona.mesh@gmail.com>
2021-03-01 22:26:57 -0800

    even odd option 2 added

--- js/04-odd-or-even.js
+++ js/04-odd-or-even.js
@@ -1,11 +1,23 @@
 /*eslint-env browser*/
-var number = 0;
-for (var number = 0; number <= 15; number+= 1) {
+/*var number = 0;
+for (number = 0; number <= 15; number+= 1) {
  if (number % 2 === 0) {
  document.write(number + " is even" + "<br>");
 } else {
     document.write(number + " is odd" + "<br>");
 }
-}
+}*/
+
+var number = 0;
+do {
+    if (number % 2 === 0) {
+        document.write(number + " is even" + "<br>");
+       } else {
+           document.write(number + " is odd" + "<br>");
+       }
+       number+= 1;
+} while (number <= 15);
+
+
 
  
